be86430a5d8109322776b62c55fb17b928375cfa
Kyle Kettler <hello@kylekettler.com>
2023-11-11 13:05:11 -0600

    updated business card layout and content

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,35 @@
 #!/usr/bin/env node
 import chalk from "chalk";
 import boxen from "boxen";
-import figlet from "figlet";
-import chalkAnimation from 'chalk-animation';
 
-const options = {
-  padding: 1,
-  width: 64,
+const intro = `Hi, I'm Kyle.
 
-  borderColor: "green",
-  titleAlignment: "center",
-  borderStyle: "double",
-}
+I am a frontend engineer with an extensive background 
+in design. I spent a decade as a designer, focused on 
+branding and UI design. In 2023, I attended a software 
+engineering boot camp and transitioned to frontend 
+engineering full-time with a focus in TypeScript, React, 
+CSS, Node.js, and relational databases.\n\n`;
 
-const name = chalkAnimation.rainbow('Kyle Kettler')
-
-const intro = `Hi, I'm Kyle Kettler.
-
-I am a ${chalk.strikethrough('designer')} frontend engineer.
-
-I've been designing and building online projects for over 10 years.\n\n`;
-
-const links = [
-  { name: chalk.hex("#fff").bgHex("#0f111a")("Website"), url: "   https://kylekettler.com" },
-  { name: chalk.hex("#fff").bgHex("#6e5494")("GitHub"), url: "    https://github.com/kyle-kettler" },
-  { name: chalk.hex("#fff").bgHex("#0072b1")("LinkedIn"), url: "   https://www.linkedin.com/in/kylekettler/" },
+const info = [
+  { name: chalk.hex("#18181b").bgHex("#e4e4e7")(" Website "), content: "   https://kylekettler.com" },
+  { name: chalk.hex("#fff").bgHex("#6e5494")(" GitHub "), content: "    https://github.com/kyle-kettler" },
+  { name: chalk.hex("#fff").bgHex("#0072b1")(" LinkedIn "), content: "  https://www.linkedin.com/in/kylekettler/" },
+  { name: chalk.hex("#fff").bgHex("#34d399")(" Email "), content: "     hello@kylekettler.com" },
+  { name: chalk.hex("#fff").bgHex("#06b6d4")(" Stack "), content: "     JS/TS, CSS, React, Node,js" },
 ]
 
-const linkList = links.map((link) => `${link.name} ${link.url}`).join("\n");
+const infoList = info.map((line) => `${line.name} ${line.content}`).join("\n");
 
-figlet.text(
-  "Kyle Kettler",
-  {
-    font: "Slant",
-    width: 80,
-    horizontalLayout: "fitted",
-  },
-  function(err, data) {
-    if (err) {
-      console.log("Something went wrong...");
-      console.dir(err);
-      return;
-    }
-    console.log(data);
-    console.log(boxen(intro + linkList, options));
-  }
-)
-// figlet("Kyle Kettler", function (err, data) {
-//   if (err) {
-//     console.log("Something went wrong...");
-//     console.dir(err);
-//     return;
-//   }
-//   console.log(data);
-//   console.log(boxen(intro + linkList, options));
-// });
+const outro = `\n 
+Feel free to reach out, I'm always down to connect!`
 
-// figlet.fonts(function (err, fonts) {
-//   if (err) {
-//     console.log("something went wrong...");
-//     console.dir(err);
-//     return;
-//   }
-//   console.dir(fonts);
-// });
+console.log(boxen(intro + infoList + outro, {
+  padding: 1,
+  margin: 1,
+  borderStyle: "double",
+  titleAlignment: "center",
+  title: "Kyle Kettler",
+  borderColor: "blueBright"
+}));
+
